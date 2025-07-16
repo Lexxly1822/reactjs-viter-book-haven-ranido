7000aacc161db0fd1fdf80dc0cc3c68ccf79fdd1
ceejaybabadilla <ceejaybabadilla2@gmail.com>
2025-07-16 15:58:41 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./components/pages/home/Home";
+import Header from "./components/pages/partial/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/home/faq/Faq.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { FaChevronUp } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa6";
+
+const Faq = () => {
+  const [activeAccordion, setActiveAccordion] = React.useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveAccordion(activeAccordion === index ? null : index);
+  };
+  console.log(activeAccordion)
+
+  return (
+    <>
+      {/* FAQ */}
+      <section id="faq" className="py-16 bg-gray-50">
+        <div className="container mx-auto px-4">
+          <div className="text-center mb-12">
+            <h2 className="text-3xl font-bold text-gray-900 mb-2">
+              Frequently Asked Question
+            </h2>
+          </div>
+          <p className="text-gray max-w-2xl mx-auto">
+            Find Answer to common question
+          </p>
+        </div>
+
+        <div className="max-w-3xl mx-auto">
+          <div className="bg-white rounded-lg shadow-md overflow-hidden mb-4">
+            <button
+              onClick={() => toggleAccordion(0)}
+              className="w-full flex justify-between items-center p-6 text-left"
+            >
+              <h3 className="font-bold text-gray-900">
+                How LONG DOES SHIPPING TAKE?
+              </h3>
+
+              {activeAccordion ===0 ? (
+                <FaChevronUp className=" text-indigo-600" />
+              ) : (
+                <FaChevronDown className="text-indigo-600"/>
+              )}
+            </button>
+            {activeAccordion === 0 && (
+                <div className="px-6 pb-6 text-gray-600">
+                    <p>Standard shipping typically takes 3-5 business day within the contimental US. </p>
+                </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Faq;
